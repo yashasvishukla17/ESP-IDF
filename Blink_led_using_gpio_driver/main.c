91a2be24e279edfcc138f8080216113125199c5c
Yashasvi <yashasvi.shukla1704@gmail.com>
2026-06-16 14:03:52 +0530

    Add files via upload

---
+++ Blink_led_using_gpio_driver/main.c
@@ -0,0 +1,19 @@
+#include <driver/gpio.h>
+#include<freertos/FreeRTOS.h>
+#include "esp_log.h"
+#include<freertos/task.h>
+#define LED_PIN 2
+
+void app_main(void){
+    gpio_reset_pin(LED_PIN); //reset the pin in the beginning
+    gpio_set_direction(LED_PIN,GPIO_MODE_OUTPUT); //pinMode
+    while(1){ //do this always
+        ESP_LOGI("MAIN","LED IS BLINKING");
+        gpio_set_level(LED_PIN,1); //digitalWrite
+        vTaskDelay(pdMS_TO_TICKS(1000)); //delay
+
+        gpio_set_level(LED_PIN,0);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
