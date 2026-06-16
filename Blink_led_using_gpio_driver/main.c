7c8741606abe398bf34dc80aeb65ca0e845b8c07
Yashasvi <yashasvi.shukla1704@gmail.com>
2026-06-16 14:04:27 +0530

    Update comments for clarity in main.c

--- Blink_led_using_gpio_driver/main.c
+++ Blink_led_using_gpio_driver/main.c
@@ -7,7 +7,7 @@
 void app_main(void){
     gpio_reset_pin(LED_PIN); //reset the pin in the beginning
     gpio_set_direction(LED_PIN,GPIO_MODE_OUTPUT); //pinMode
-    while(1){ //do this always
+    while(1){ //kinda like loop
         ESP_LOGI("MAIN","LED IS BLINKING");
         gpio_set_level(LED_PIN,1); //digitalWrite
         vTaskDelay(pdMS_TO_TICKS(1000)); //delay
